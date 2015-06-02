d9656b4c264e6bded0814d54dba9ea4a7d03b484
Zir01 <bruce@id.com.au>
2015-06-02 17:06:26 +1000

    Fixed minor bug with postCreator. Now creates valid path and signature

--- lib/Creators/postCreator.ts
+++ lib/Creators/postCreator.ts
@@ -1,9 +1,45 @@
-﻿class postCreator {
+﻿import _ = require("lodash");
+
+class postCreator {
     static create(signature: ISignatureDefinition): string {
+
+        
         var signatureText = "";
         signatureText += "\t" + signature.signature.replace(";", "{") + "\n";
-        signatureText += "\t\tvar fullPath = this.host + '" + signature.path + "';\n";
-        signatureText += "\t\treturn this.httpPost(fullPath, " + signature.parameters[0].name + ");\n";
+        signatureText += "\t\tvar path = this.host + '" + signature.path + "';\n";
+
+
+        var query = "";
+        _.forEach(signature.parameters, (p: IParamDefinition, i: number) => {
+            if (p.i_n == "path")
+                signatureText += "\t\tpath = path.replace('{" + p.name.trim() + "}', " + p.name + ".toString());\n";
+            if (p.i_n == "query") {
+
+                if (p.type != "array") {
+                    if (i == 0)
+                        query += "\t\tpath += '?" + p.name + "=' + " + p.name + ";\n";
+                    else
+                        query += "\t\tpath += '&" + p.name + "=' + " + p.name + ";\n";
+                } else {
+                    query += "\t\tvar qs = '';\n";
+                    query += "\t\tfor (var i = 0; i < " + p.name + ".length; i++) {\n";
+                    query += "\t\t\tqs += '" + p.name + "=' + " + p.name + "[i] + '&'\n";
+                    query += "\t\t}\n";
+                    query += "\t\tif (qs.length > 0) path += '?' + qs;\n";
+                }
+            }
+        });
+        if (query.length > 0) {
+            signatureText += query;
+        }
+
+
+        // get the in body parameter
+        var bodyparam = _.find(signature.parameters, (p: IParamDefinition) => {
+            return p.i_n == "body";
+        });
+
+        signatureText += "\t\treturn this.httpPost(path, " + bodyparam.name + ");\n";
         signatureText += "\t}\n\n";
         return signatureText;
     }
