172b5dc5a72b33685900f94a644f6c6d1924e73a
Jais Pedersen <jais@pedersens.net>
2015-08-12 12:42:47 +0700

    Move common creator code to creatorHelper

--- lib/Creators/getCreator.ts
+++ lib/Creators/getCreator.ts
@@ -1,46 +1,14 @@
-﻿import _ = require("lodash");
+﻿import helper = require("./creatorHelper");
 
-class getCreator{
+class getCreator {
     static create(signature: ISignatureDefinition): string {
- 
-        var impText = "";
-
-        impText += "\t" + signature.signature.replace(";", "{") + "\n";
-        impText += "\t\tvar path='" + signature.path + "'\n\n";
-
-        // logic to create overload checks on parameters
-
-        var query = "";
-        var queryCnt = 0;
-        _.forEach(signature.parameters, (p: IParamDefinition, i: number) => {
-            if (p.i_n == "path")
-                impText += "\t\tpath = path.replace('{" + p.name.trim() + "}', " + p.name + ".toString());\n";
-            if (p.i_n == "query") {
-
-                if (p.type != "array") {
-                    if (queryCnt == 0)
-                        query += "\t\tpath += '?" + p.name + "=' + " + p.name + ";\n";
-                    else
-                        query += "\t\tpath += '&" + p.name + "=' + " + p.name + ";\n";
-                } else {
-                    query += "\t\tvar qs = '';\n";
-                    query += "\t\tfor (var i = 0; i < " + p.name + ".length; i++) {\n";
-                    query += "\t\t\tqs += '" + p.name + "=' + " + p.name + "[i] + '&'\n";
-                    query += "\t\t}\n";
-                    query += "\t\tif (qs.length > 0) path += '?' + qs;\n";
-                }
-                queryCnt++;
-            }
-        });
-        if (query.length > 0) {
-            impText += query;
-        }
-
-
-        impText += "\t\tvar fullPath = this.host + path;\n";
-        impText += "\t\treturn this.httpGet(fullPath);\n";
-        impText += "\t}\n";
-        return impText;
+        var result: string = "";
+        result += "\n\t" + signature.signature.replace(";", " {") + "\n";
+        result += helper.generatePathFromSignature(signature);
+        result += "\t\treturn this.httpGet(path);\n";
+        result += "\t}\n";
+        return result;
     }
 }
+
 export = getCreator;
--- lib/Creators/postCreator.ts
+++ lib/Creators/postCreator.ts
@@ -1,38 +1,10 @@
-﻿import _ = require("lodash");
+﻿import _      = require("lodash");
+import helper = require("./creatorHelper");
 
 class postCreator {
     static create(signature: ISignatureDefinition): string {
-
-        
-        var signatureText = "";
-        signatureText += "\t" + signature.signature.replace(";", "{") + "\n";
-        signatureText += "\t\tvar path = this.host + '" + signature.path + "';\n";
-
-
-        var query = "";
-        _.forEach(signature.parameters, (p: IParamDefinition, i: number) => {
-            if (p.i_n == "path")
-                signatureText += "\t\tpath = path.replace('{" + p.name.trim() + "}', " + p.name + ".toString());\n";
-            if (p.i_n == "query") {
-
-                if (p.type != "array") {
-                    if (i == 0)
-                        query += "\t\tpath += '?" + p.name + "=' + " + p.name + ";\n";
-                    else
-                        query += "\t\tpath += '&" + p.name + "=' + " + p.name + ";\n";
-                } else {
-                    query += "\t\tvar qs = '';\n";
-                    query += "\t\tfor (var i = 0; i < " + p.name + ".length; i++) {\n";
-                    query += "\t\t\tqs += '" + p.name + "=' + " + p.name + "[i] + '&'\n";
-                    query += "\t\t}\n";
-                    query += "\t\tif (qs.length > 0) path += '?' + qs;\n";
-                }
-            }
-        });
-        if (query.length > 0) {
-            signatureText += query;
-        }
-
+        var result = "\n\t" + signature.signature.replace(";", " {") + "\n";
+        result += helper.generatePathFromSignature(signature);
 
         // get the in body parameter
         var bodyparam = _.find(signature.parameters, (p: IParamDefinition) => {
@@ -40,13 +12,14 @@ class postCreator {
         });
 
         if (bodyparam) {
-            signatureText += "\t\treturn this.httpPost(path, " + bodyparam.name + ");\n";
+            result += "\t\treturn this.httpPost(path, " + bodyparam.name + ");\n";
         } else {
-            signatureText += "\t\treturn this.httpPost(path, null);\n";
+            result += "\t\treturn this.httpPost(path, null);\n";
         }
 
-        signatureText += "\t}\n\n";
-        return signatureText;
+        result += "\t}\n\n";
+        return result;
     }
 }
+
 export = postCreator;
