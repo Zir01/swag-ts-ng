4f4251c6bbad15ff10b7f5c6305eb3ac154d8fbe
Jais Pedersen <jais@pedersens.net>
2015-08-12 14:56:35 +0700

    Add support for delete and put requests

---
+++ lib/Creators/deleteCreator.ts
@@ -0,0 +1,14 @@
+﻿import helper = require("./creatorHelper");
+
+class deleteCreator {
+    static create(signature: ISignatureDefinition): string {
+        var result: string = "";
+        result += "\n\t" + signature.signature.replace(";", " {") + "\n";
+        result += helper.generatePathFromSignature(signature);
+        result += "\t\treturn this.httpDelete(path);\n";
+        result += "\t}\n";
+        return result;
+    }
+}
+
+export = deleteCreator;
