fb493205755687207debb350e0901e5ee9d607f0
Zir01 <bruce@id.com.au>
2015-08-21 09:19:32 +1000

    bumped dist to next version for publish to npm

--- dist/app.js
+++ dist/app.js
@@ -2,33 +2,37 @@ var swaggerService = require("./lib/SwaggerService");
 var http = require("http");
 exports.process = function (options) {
     if (!options) {
-        console.error("Sorry. Please supply options with swaggerPath and destination properties");
+        console.error("Sorry. Please supply options with swaggerPath");
     }
     else {
         http.get(options.swaggerPath, function (res) {
-            res.setEncoding('utf-8');
-            var swaggerString = '';
-            res.on('data', function (data) {
-                console.log('Swagger json found!');
+            res.setEncoding("utf-8");
+            var swaggerString = "";
+            res.on("data", function (data) {
+                console.log("Swagger json found!");
                 swaggerString += data;
             });
-            res.on('end', function () {
+            res.on("end", function () {
                 // swagger object coming from server
                 var swaggerObject = JSON.parse(swaggerString);
-                var opt = {
-                    destination: options.destination,
-                    swaggerObject: swaggerObject
-                };
+                var opt = options;
+                opt.swaggerObject = swaggerObject;
+                // for backwards compatibility
                 if (options.moduleName) {
-                    opt.moduleName = options.moduleName;
+                    opt.clientModuleName = options.moduleName;
+                    opt.modelModuleName = options.moduleName;
+                }
+                // for backwards compatibility
+                if (options.destination) {
+                    opt.clientDestination = options.destination;
+                    opt.interfaceDestination = options.destination;
                 }
                 var swagSrv = new swaggerService(opt);
                 swagSrv.process();
             });
-        }).on('error', function (e) {
-            console.log('Error : ' + e.message);
+        }).on("error", function (e) {
+            console.log("Error : " + e.message);
         });
-        ;
     }
 };
 //# sourceMappingURL=app.js.map
