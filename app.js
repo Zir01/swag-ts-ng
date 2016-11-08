cf9404090607cce70209b5fba0a5e7dfcedcf682
Zir01 <bruce@id.com.au>
2016-11-08 22:06:35 +1100

    merge

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
+"use strict";
+require('source-map-support/register'); //to get ts stacktraces
 var swaggerService = require("./lib/SwaggerService");
 var http = require("http");
+var fs = require("fs");
 exports.process = function (options) {
+    function runConversion(swaggerString) {
+        var swaggerObject = JSON.parse(swaggerString);
+        var opt = options;
+        opt.swaggerObject = swaggerObject;
+        // for backwards compatibility
+        if (options.moduleName) {
+            opt.clientModuleName = options.moduleName;
+            opt.modelModuleName = options.moduleName;
+        }
+        // for backwards compatibility
+        if (options.destination) {
+            opt.clientDestination = options.destination;
+            opt.interfaceDestination = options.destination;
+        }
+        var swagSrv = new swaggerService(opt);
+        swagSrv.process();
+    }
     if (!options) {
         console.error("Sorry. Please supply options with swaggerPath");
     }
     else {
-        http.get(options.swaggerPath, function (res) {
-            res.setEncoding("utf-8");
-            var swaggerString = "";
-            res.on("data", function (data) {
-                console.log("Swagger json found!");
-                swaggerString += data;
+        if (options.swaggerLocation == "url") {
+            http.get(options.swaggerPath, function (res) {
+                res.setEncoding("utf-8");
+                var swaggerString = "";
+                res.on("data", function (data) {
+                    console.log("Swagger json found!");
+                    swaggerString += data;
+                });
+                res.on("end", function () {
+                    // swagger object coming from server
+                    runConversion(swaggerString);
+                });
+            }).on("error", function (e) {
+                console.log("Error : " + e.message);
             });
-            res.on("end", function () {
-                // swagger object coming from server
-                var swaggerObject = JSON.parse(swaggerString);
-                var opt = options;
-                opt.swaggerObject = swaggerObject;
-                // for backwards compatibility
-                if (options.moduleName) {
-                    opt.clientModuleName = options.moduleName;
-                    opt.modelModuleName = options.moduleName;
-                }
-                // for backwards compatibility
-                if (options.destination) {
-                    opt.clientDestination = options.destination;
-                    opt.interfaceDestination = options.destination;
-                }
-                var swagSrv = new swaggerService(opt);
-                swagSrv.process();
-            });
-        }).on("error", function (e) {
-            console.log("Error : " + e.message);
-        });
+        }
+        else {
+            //assuming 'local' swagger files.
+            //usying sync. no use doing async stuff
+            runConversion(fs.readFileSync(options.swaggerPath, 'UTF-8'));
+        }
     }
 };
 //# sourceMappingURL=app.js.map
--- dist/app.js
+++ dist/app.js
@@ -2,36 +2,45 @@
 require('source-map-support/register');
 const swaggerService = require("./lib/SwaggerService");
 const http = require("http");
+const fs = require("fs");
 exports.process = (options) => {
+    function runConversion(swaggerString) {
+        var swaggerObject = JSON.parse(swaggerString);
+        var opt = options;
+        opt.swaggerObject = swaggerObject;
+        if (options.moduleName) {
+            opt.clientModuleName = options.moduleName;
+            opt.modelModuleName = options.moduleName;
+        }
+        if (options.destination) {
+            opt.clientDestination = options.destination;
+            opt.interfaceDestination = options.destination;
+        }
+        var swagSrv = new swaggerService(opt);
+        swagSrv.process();
+    }
     if (!options) {
         console.error("Sorry. Please supply options with swaggerPath");
     }
     else {
-        http.get(options.swaggerPath, (res) => {
-            res.setEncoding("utf-8");
-            var swaggerString = "";
-            res.on("data", (data) => {
-                console.log("Swagger json found!");
-                swaggerString += data;
+        if (options.swaggerLocation == "url") {
+            http.get(options.swaggerPath, (res) => {
+                res.setEncoding("utf-8");
+                var swaggerString = "";
+                res.on("data", (data) => {
+                    console.log("Swagger json found!");
+                    swaggerString += data;
+                });
+                res.on("end", () => {
+                    runConversion(swaggerString);
+                });
+            }).on("error", (e) => {
+                console.log("Error : " + e.message);
             });
-            res.on("end", () => {
-                var swaggerObject = JSON.parse(swaggerString);
-                var opt = options;
-                opt.swaggerObject = swaggerObject;
-                if (options.moduleName) {
-                    opt.clientModuleName = options.moduleName;
-                    opt.modelModuleName = options.moduleName;
-                }
-                if (options.destination) {
-                    opt.clientDestination = options.destination;
-                    opt.interfaceDestination = options.destination;
-                }
-                var swagSrv = new swaggerService(opt);
-                swagSrv.process();
-            });
-        }).on("error", (e) => {
-            console.log("Error : " + e.message);
-        });
+        }
+        else {
+            runConversion(fs.readFileSync(options.swaggerPath, 'UTF-8'));
+        }
     }
 };
 
